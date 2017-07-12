9823145fbe02e72e0ae9305d7b657080e2a067cc
Lynn Long Jr <lynnlong000@gmail.com>
2017-07-12 22:47:10 +0000

    change my todo functions in the object within version_3.js

--- version_3.js
+++ version_3.js
@@ -1,22 +1,25 @@
 /* the teacher is using plunker but i'd rather stay in my C9
 hopefully that is not a issue 
 */
-
+// files says version 3 but i'm im branch four
 var todoList = {
-    todos:['item 1', 'item 2', 'item 3'],
+    todos:[],
     displayTodos:function(){
         console.log('My Todos:',this.todos());
     },
-    addTodo:function(todo){
-        this.todos.push(todo);
+    addTodo:function(todoText){
+        this.todos.push({
+            todoText:todoText,    //todoText(this is the key):todoText(this is the variable for the value)
+            completed:false
+        });
         this.displayTodos();
     },
-    changeTodo:function(position,newValue){
-        this.todos[position] = newValue;
+    changeTodo:function(position,todoText){
+        this.todos[position].todoText = 'todoText';
         this.displayTodos();
     },
     deleteTodos:function (position){
         this.todos.splice(position,1);
         this.displayTodos();
     } 
-}
+};
