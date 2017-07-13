31cbcbe34c44a2687a7c1cd7c6e7deab7c4a3642
Lynn Long Jr <lynnlong000@gmail.com>
2017-07-13 00:03:50 +0000

    great session on loops. also might of deleted some files i wanted hoping this works

--- version_3.js
+++ version_3.js
@@ -5,7 +5,19 @@ hopefully that is not a issue
 var todoList = {
     todos:[],
     displayTodos:function(){
-        console.log('My Todos:',this.todos());
+        if(this.todos.length === 0){
+            console.log("your todo list is empty");
+        } else {
+            console.log('My Todos:');
+            for(var i =0;i<this.todos.length;i++){
+                console.log(this.todos[i].todoText);
+                if(this.todos[i].completed === true){
+                    console.log('(x)',this.todos[i].todoText);
+                } else {
+                    console.log('( )',this.todos[i].todoText);
+                }
+            }
+        }             
     },
     addTodo:function(todoText){
         this.todos.push({
@@ -21,5 +33,10 @@ var todoList = {
     deleteTodos:function (position){
         this.todos.splice(position,1);
         this.displayTodos();
-    } 
+    },
+    toggleCompleted:function(position){
+        var todo = this.todos[position];  // made a variable for less typing
+        todo.completed = !todo.completed; // ! is the inverse of the set boolean value, called the bang operator
+        this.displayTodos();
+    }
 };
