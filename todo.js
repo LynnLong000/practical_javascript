b301d6ba31d425c9073ab4e40811fb8886d4cd5d
Lynn Long Jr <lynnlong000@gmail.com>
2017-07-17 18:28:33 +0000

    updated index.html and todo.js plus finished the version 9 and got it working out the consolewhich is the best part

--- todo.js
+++ todo.js
@@ -1,43 +1,24 @@
 /* the teacher is using plunker but i'd rather stay in my C9
 hopefully that is not a issue 
 */
-// version 8
+// version 9 deleted todolist.displaytodos
 var todoList = {
     todos:[],
-    displayTodos:function(){
-        if(this.todos.length === 0){
-            console.log("your todo list is empty");
-        } else {
-            console.log('My Todos:');
-            for(var i =0;i<this.todos.length;i++){
-                console.log(this.todos[i].todoText);
-                if(this.todos[i].completed === true){
-                    console.log('(x)',this.todos[i].todoText);
-                } else {
-                    console.log('( )',this.todos[i].todoText);
-                }
-            }
-        }             
-    },
     addTodo:function(todoText){
         this.todos.push({
             todoText:todoText,    //todoText(this is the key):todoText(this is the variable for the value)
             completed:false
         });
-        this.displayTodos();
     },
     changeTodo:function(position,todoText){
-        this.todos[position].todoText = 'todoText';
-        this.displayTodos();
+        this.todos[position].todoText = todoText;
     },
-    deleteTodos:function (position){
+    deleteTodo:function (position){
         this.todos.splice(position,1);
-        this.displayTodos();
     },
     toggleCompleted:function(position){
         var todo = this.todos[position];  // made a variable for less typing
         todo.completed = !todo.completed; // ! is the inverse of the set boolean value, called the bang operator
-        this.displayTodos();
     },
     toggleAll: function(){
         var totalTodos = this.todos.length;
@@ -45,7 +26,7 @@ var todoList = {
         
         // Get number of completed todos
         for(var i=0;i< totalTodos;i++){
-            if(this.todos[i] === true){
+            if(this.todos[i].completed === true){
                 completedTodos++;
             }
         }
@@ -55,22 +36,19 @@ var todoList = {
                 this.todos[i].completed = false;
             }
         } else{ // Otherwise, make everything true
-                for (var k = 0 ; k < totalTodos;k++){
-                    this.todos[k].completed === false; 
+                for (var i = 0 ; i < totalTodos;i++){
+                    this.todos[i].completed = true; 
                 }
         }
-        this.displayTodos();
     }
 };
-
+/* added view.displayTodos()*/
 var handlers ={
-    displayTodos:function(){
-        todoList.displayTodos();
-    },
     addTodo:function(){
         var addTodoTextInput = document.getElementById('addTodoTextInput');
         todoList.addTodo(addTodoTextInput.value);
         addTodoTextInput.value = '';
+        view.displayTodos();
     },
     changeTodo:function(){
         var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
@@ -78,34 +56,50 @@ var handlers ={
         todoList.changeTodo(changeTodoPositionInput.valueAsNumber,changeTodoTextInput.value);
         changeTodoPositionInput.value ='';
         changeTodoTextInput.value = '';
+        view.displayTodos();
     },
-    deleteTodos:function(){
+    deleteTodo:function(){
         var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
         todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
         deleteTodoPositionInput.value ='';
+        view.displayTodos();
     },
     toggleCompleted:function(){
         var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
         todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
         toggleCompletedPositionInput.value='';
+        view.displayTodos();
     },
     toggleAll:function(){
         todoList.toggleAll();
+        view.displayTodos();
     }
 };
 
-// the difference is the top code and short and neat; bottom code is more drawn out
+// display whats going on 
 
-
-/*   refactoring this code above
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function(){
-    todoList.displayTodos();  
-});
-
-toggleAllButton.addEventListener('click',function(){
-    todoList.toggleAll();
-});
-*/
+var view = {
+    displayTodos:function(){
+         var todosUl = document.querySelector('ul');
+         todosUl.innerHTML = '';
+        for(var i=0;i<todoList.todos.length;i++){
+            var todoLi = document.createElement('li');
+            var todo = todoList.todos[i];
+            // '(x)' todoText;
+            // var todoTextWithCompleted = '' ;
+            //if (todo.completed === true)
+                //(x)todoText 
+            // else
+             //( )todoText
+            var todoTextWithCompletion ="";
+            if(todo.completed === true){
+                todoTextWithCompletion = '(x)' + todo.todoText;
+            } else {
+                todoTextWithCompletion= '( )' + todo.todoText;
+            }
+            
+            todoLi.textContent = todoTextWithCompletion;
+            todosUl.appendChild(todoLi);
+        }
+    }
+};
---
+++ version_js/learning_LI.js
@@ -0,0 +1,5 @@
+var todoLi =  document.createElement('li');
+
+var todosUl = document.querySelector('ul');
+
+todosUl.appendChild(todoLi);
